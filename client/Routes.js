49fc437cf6a0ab6601f288e978b8e440cd5372cf
armaandh <dhanjiarmaan@gmail.com>
2018-09-13 20:04:58 -0700

    clean wipe

--- client/Routes.js
+++
@@ -1,9 +0,0 @@
-import loadable from "loadable-components";
-
-export const Index = loadable(() => import("./components/Index"));
-export const Home = loadable(() => import("./components/Home"));
-export const PageNotFound = loadable(() => import("./components/PageNotFound"));
-export const About = loadable(() => import("./components/About"));
-export const Signup = loadable(() => import("./components/Signup"));
-export const Login = loadable(() => import("./components/Login"));
-
--- client/components/Index.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from "react";
-
-class Index extends Component {
-  render() {
-    return <h1>Interline Reader</h1>;
-  }
-}
-
-export default Index;
