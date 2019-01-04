73e6800f6fe9fae39e783bf8bdd8d0f201ba05ff
Armaan Dhanji <dhanjiarmaan@gmail.com>
2019-01-04 03:14:25 -0800

    changed default dictionary to be Daum dictionary

--- client/src/components/common/sideBar/components/Dictionary/Dictionary.js
+++ client/src/components/common/sideBar/components/Dictionary/Dictionary.js
@@ -6,8 +6,8 @@ import {getVocabforStory} from '../../../../../actions/vocab';
 class Dictionary extends Component {
   render(){
     return(
-      <div style={{height: "40vh"}}>
-        <iframe title={'dictionaryIframe'} style={{width: "100%", height: "100%"}} className="frame" src="https://krdict.korean.go.kr/eng/smallDic/mainAction?nationCode=6"></iframe>
+      <div style={{height: "100vh"}}>
+        <iframe title={'dictionaryIframe'} style={{width: "100%", height: "100%"}} className="frame" src="https://small.dic.daum.net/search.do?q=%EA%B0%90%EC%9E%90&dic=eng"></iframe>
       </div>
     )
   }
