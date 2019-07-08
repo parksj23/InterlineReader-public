bc1842c34a4c49529caf19bf78d108dfb0190db9
Armaan Dhanji <dhanjiarmaan@gmail.com>
2019-07-08 06:17:52 -0700

    prod keys

--- server/config/prod.js
+++ server/config/prod.js
@@ -9,5 +9,6 @@ module.exports = {
   mongoURI: 'mongodb://ubcreader:britishcolumbia1324!@ds155862.mlab.com:55862/ubcreadertesting',
   secretOrKey: 'secret',
   sendGridKey: 'YOUR_SENDGRID_KEY',
+  mongoAnalyticsURI: 'mongodb://ubcreader:britishcolumbia1324!@ds119014.mlab.com:19014/ubcreader',
   redirectDomain: 'http://127.0.0.1:5050/', // e.g. http://localhost:3000
 };
