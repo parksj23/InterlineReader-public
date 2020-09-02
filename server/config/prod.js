e3419cd447b6050c1579df79b1be54ad9315819a
Alfred Hong <alf.hong91@gmail.com>
2020-09-01 21:34:14 -0700

    modified production database urls

--- server/config/prod.js
+++ server/config/prod.js
@@ -6,21 +6,21 @@
 };*/
 
 //Actual prod environment - DON"T DELETE
-module.exports = {
+/*module.exports = {
   mongoURI: 'mongodb+srv://ubcreader:britishcolumbia1324!@cluster0-j9y3z.mongodb.net/InterlineReaderDB?retryWrites=true&w=majority',
   secretOrKey: 'secret',
   sendGridKey: 'YOUR_SENDGRID_KEY',
   mongoAnalyticsURI: 'mongodb://ubcreader:britishcolumbia1324!@ds119014.mlab.com:19014/ubcreader',
   redirectDomain: 'http://127.0.0.1:5050/',
   databaseName: 'InterlineReaderDB'
-};
+};*/
 
 //FOR v1.5
-/*module.exports = {
-  mongoURI: 'mongodb+srv://ubcreader:britishcolumbia1324!@cluster0-cgsxj.mongodb.net/interlineReaderTestDb?retryWrites=true&w=majority',
+module.exports = {
+  mongoURI: 'mongodb+srv://ubcreader:britishcolumbia1324!@korn410.bqxsd.mongodb.net/InterlineReaderKorn410DB?retryWrites=true&w=majority',
   secretOrKey: 'secret',
   sendGridKey: 'YOUR_SENDGRID_KEY',
   redirectDomain: 'http://127.0.0.1:5050/', // e.g. http://localhost:3000
   mongoAnalyticsURI: 'mongodb://ubcreader:britishcolumbia1324!@ds119014.mlab.com:19014/ubcreader',
-  databaseName: 'interlineReaderTestDb'
-};*/
+  databaseName: 'InterlineReaderKorn410DB'
+};
