5de8cb1ecb01aa87d14c19164b385f9ea2d352b1
Alfred Hong <alf.hong91@gmail.com>
2020-09-01 21:12:55 -0700

    striped off non Korn410 stuff

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,128 +1,126 @@
 import React, { Component } from 'react';
-import { Link, withRouter } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { logoutUser } from '../../actions/auth';
 import {updateDrawerSize} from '../../actions/dashboard';
 import {toggleSideBar} from "../../actions/sideBar";
 import SideBarButton from './common/sideBarButton';
-import './style/navbar.css';
 
 class Navbar extends Component {
-    onLogoutClick = (e) => {
-        e.preventDefault();
-        this.props.logoutUser();
+  onLogoutClick = (e) => {
+    e.preventDefault();
+    this.props.logoutUser();
+  }
+
+  toggleDrawer = (side, open) => () => {
+    let width = document.getElementById('resizeContainer') ? document.getElementById('resizeContainer').offsetWidth - parseInt(window.getComputedStyle(document.getElementById('mainContainer')).marginLeft,10) : "30vw"
+    let height = document.getElementById('resizeContainer') ? document.getElementById('resizeContainer').clientHeight : "100vh"
+    let size = {
+      width,
+      height
     }
 
-    toggleDrawer = (side, open) => () => {
-        let width = document.getElementById('resizeContainer') ? document.getElementById('resizeContainer').offsetWidth - parseInt(window.getComputedStyle(document.getElementById('mainContainer')).marginLeft,10) : "30vw"
-        let height = document.getElementById('resizeContainer') ? document.getElementById('resizeContainer').clientHeight : "100vh"
-        let size = {
-            width,
-            height
-        }
+    this.props.toggleSideBar(open, size);
+    this.setState({
+      [side]: open,
+    });
+  };
 
-        this.props.toggleSideBar(open, size);
-        this.setState({
-            [side]: open,
-        });
-    };
+  render() {
+    const { isAuthenticated, user } = this.props.auth;
+    const authLinks = (
+      <ul className="navbar-nav ml-auto">
+        <li className="nav-item">
+          <a
+            href=""
+            className="nav-link"
+            onClick={this.onLogoutClick}
+          >
+            Logout
+          </a>
+        </li>
+      </ul>
+    );
 
-    render() {
-        const currentPath = this.props.location.pathname;
-        const { isAuthenticated, user } = this.props.auth;
-        const authLinks = (
-            <ul className="navbar-nav ml-auto">
+    const guestLinks = (
+      <ul className="navbar-nav ml-auto">
+        <li className="nav-item">
+          <Link className="nav-link" to="/register">
+            Sign Up
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link className="nav-link" to="/login">
+            Login
+          </Link>
+        </li>
+      </ul>
+    );
+
+    return (
+      <div>
+        <nav className="navbar navbar-expand-md navbar-dark bg-dark mb-4">
+          <SideBarButton toggleDrawer={this.toggleDrawer} isDisabled={this.props.sideBar.isButtonDisabled}/>
+          <div className="container">
+            <Link className="navbar-brand" to="/">
+              Interline Reader
+            </Link>
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-toggle="collapse"
+              data-target="#mobile-nav"
+            >
+              <span className="navbar-toggler-icon" />
+            </button>
+            <div className="collapse navbar-collapse" id="mobile-nav">
+              <ul className="navbar-nav mr-auto">
                 <li className="nav-item">
-                    <a
-                        href=""
-                        className="nav-link"
-                        onClick={this.onLogoutClick}
-                    >
-                        Logout
-                    </a>
-                </li>
-            </ul>
-        );
-
-        const guestLinks = (
-            <ul className="navbar-nav ml-auto">
-                <li className="nav-item">
-                    <Link className="nav-link" to="/register">
-                        Sign Up
-                    </Link>
+                  <Link className="nav-link" to="/dashboard/modernKorean">
+                    Modern korean
+                  </Link>
                 </li>
                 <li className="nav-item">
-                    <Link className="nav-link" to="/login">
-                        Login
-                    </Link>
+                  <Link className="nav-link" to="/dashboard/KORN410">
+                    KORN 410
+                  </Link>
                 </li>
-            </ul>
-        );
-
-        return (
-            <div>
-                <nav className="navbar navbar-expand-md navbar-dark bg-dark">
-                    <SideBarButton toggleDrawer={this.toggleDrawer} isDisabled={this.props.sideBar.isButtonDisabled}/>
-                    <div className="container">
-                        <Link className="navbar-brand" to="/">
-                            Interline Reader
-                        </Link>
-                        <button
-                            className="navbar-toggler"
-                            type="button"
-                            data-toggle="collapse"
-                            data-target="#mobile-nav"
-                        >
-                            <span className="navbar-toggler-icon" />
-                        </button>
-                        <div className="collapse navbar-collapse" id="mobile-nav">
-                            <ul className="navbar-nav mr-auto">
-                                <li className="nav-item">
-                                    <Link className={currentPath==="/dashboard/modernKorean"? "selected": "nav-link"} to="/dashboard/modernKorean">
-                                        Modern Korean
-                                    </Link>
-                                </li>
-                                <li className="nav-item">
-                                    <Link className={currentPath==="/dashboard/KORN410"? "selected": "nav-link"} to="/dashboard/KORN410">
-                                        KORN 410
-                                    </Link>
-                                </li>
-                                <li className="nav-item">
-                                    <Link className={currentPath==="/about"? "selected": "nav-link"} to="/about">
-                                        About
-                                    </Link>
-                                </li>
-                                {(Object.keys(user).length>=1 && !user.isStudent) && (
-                                    <li className="nav-item">
-                                        <Link className={currentPath.includes("/instructor")? "selected": "nav-link"} to="/instructor"> Instructor Panel</Link>
-                                    </li>)
-                                }
-                            </ul>
-                            {isAuthenticated ? authLinks : guestLinks}
-                        </div>
-                    </div>
-                </nav>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/about">
+                    About
+                  </Link>
+                </li>
+                {(Object.keys(user).length>=1 && !user.isStudent) && (
+                  <li className="nav-item">
+                    <Link className="nav-link" to="/instructor"> Instructor Panel</Link>
+                  </li>)
+                }
+              </ul>
+              {isAuthenticated ? authLinks : guestLinks}
             </div>
-        );
-    }
+          </div>
+        </nav>
+      </div>
+    );
+  }
 }
 
 Navbar.propTypes = {
-    logoutUser: PropTypes.func.isRequired,
-    auth: PropTypes.object.isRequired
+  logoutUser: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired
 };
 
 const mapStateToProps = state => ({
-    auth: state.auth,
-    stories: state.stories,
-    sideBar: state.sideBar
+  auth: state.auth,
+  stories: state.stories,
+  sideBar: state.sideBar
 });
 
 const mapDispatchToPRops = {
-    logoutUser,
-    toggleSideBar,
-    updateDrawerSize
+  logoutUser,
+  toggleSideBar,
+  updateDrawerSize
 }
 
-export default withRouter(connect(mapStateToProps, mapDispatchToPRops)(Navbar));
+export default connect(mapStateToProps, mapDispatchToPRops)(Navbar);
--- client/src/components/stories/storiesContainer.js
+++ client/src/components/stories/storiesContainer.js
@@ -1,290 +1,251 @@
 import React, {Component} from "react";
 import {connect} from "react-redux";
-import {
-    getVocabforStory,
-    initStory,
-    leaveStories,
-    resetSTories,
-    enableLoading,
-    disableLoading,
-    saveHypothesisLink
-} from '../../actions/stories';
+import {getVocabforStory, initStory, leaveStories, resetSTories, enableLoading, disableLoading,saveHypothesisLink} from '../../actions/stories';
 import {endGrammarSearchSession} from '../../actions/analytics'
 
 import {
-    getListOfSavedWords,
-    getListOfSavedGrammars,
-    getSavedWords,
-    enableSideBarButton,
-    resetSideBar,
-    updateSavedWords
+  getListOfSavedWords,
+  getSavedWords,
+  enableSideBarButton,
+  resetSideBar,
+  updateSavedWords
 } from "../../actions/sideBar";
 import {disableSideBarButton} from '../../actions/dashboard';
 import './styles/stories.css';
 import Story from './components/story';
 import SideBar from '../common/sideBar/sideBarContainer'
-import {ClipLoader} from 'react-spinners';
+import { ClipLoader } from 'react-spinners';
 import Modal from '@material-ui/core/Modal'
 import FlashCardsContainer from './components/common/FlashCard/FlashCardsContainer';
 import OriginalText from './components/common/OriginalText/OriginalTextContainer';
 
 class StoriesContainer extends Component {
-    constructor(props) {
-        super(props)
+  constructor(props){
+    super(props)
 
-        this.state = {
-            applicationDescription: "",
-            storyTitle: "",
-            selectedLanguage: "",
-            storyInfo: null,
-            showModal: false,
-            isSpeedDialOpen: false,
-            modalComponent: "",
-            savedVocabs: [],
-            openFiltered: false
-        }
-        this.handleTranslate.bind(this)
-        this.handleFlashCards.bind(this)
-        this.handleOriginalText.bind(this)
+    this.state = {
+      applicationDescription: "",
+      storyTitle: "",
+      selectedLanguage: "",
+      storyInfo: null,
+      showModal: false,
+      isSpeedDialOpen: false,
+      modalComponent: "",
     }
+    this.handleTranslate.bind(this)
+    this.handleFlashCards.bind(this)
+    this.handleOriginalText.bind(this)
+  }
 
 
-    componentWillMount() {
-        this.props.enableLoading();
-        const search = this.props.location.search;
 
-        const params = new URLSearchParams(search);
-        const storyTitle = params.get('storyTitle'); // bar
-        this.setState({
-            storyTitle,
-        })
+  componentWillMount() {
+    this.props.enableLoading();
+    const search = this.props.location.search;
+
+    const params = new URLSearchParams(search);
+    const storyTitle = params.get('storyTitle'); // bar
+    this.setState({
+      storyTitle,
+    })
+  }
+
+  componentDidMount() {
+    let iframes = [].slice.apply(document.querySelectorAll('iframe'));
+    for (let anIframe of iframes) {
+      if (anIframe.src.startsWith("https://hypothes.is")) {
+        anIframe.src = anIframe.src.split("&")[0]
+      }
     }
-
-    componentDidMount() {
-        let iframes = [].slice.apply(document.querySelectorAll('iframe'));
-        for (let anIframe of iframes) {
-            if (anIframe.src.startsWith("https://hypothes.is")) {
-                anIframe.src = anIframe.src.split("&")[0]
-            }
-        }
-        let storyInfo;
-        this.props.initStory(this.state.storyTitle).then(resp => {
-            storyInfo = resp.storyInfo
-            this.props.getListOfSavedWords(this.props.userId, storyInfo._id).then(resp => {
-                this.setState({
-                    savedVocabs: resp.savedVocabIds
-                });
-                this.props.enableSideBarButton();
-                this.props.getSavedWords(this.props.userId, storyInfo._id, resp.savedVocabIds, this.props.stories.selectedLanguage).then(resp => {
-                    this.props.disableLoading();
-                    this.setState({
-                        selectedLanguage: this.props.stories.selectedLanguage
-                    })
-
-                });
+    let storyInfo;
+    this.props.initStory(this.state.storyTitle).then(resp => {
+        storyInfo = resp.storyInfo
+        this.props.getListOfSavedWords(this.props.userId, storyInfo._id).then(resp => {
+          this.props.enableSideBarButton();
+          this.props.getSavedWords(this.props.userId, storyInfo._id, resp.savedVocabIds, this.props.stories.selectedLanguage).then(resp => {
+            this.props.disableLoading();
+            this.setState({
+              selectedLanguage: this.props.stories.selectedLanguage
             })
-            this.props.getListOfSavedGrammars(this.props.userId, storyInfo._id);
-        });
-    }
 
-    handleTranslate = (language) => {
-        this.setState({
-            selectedLanguage: language,
-            isSpeedDialOpen: false
+          })
         })
+    });
+  }
+
+  handleTranslate = (language) => {
+    this.setState({
+      selectedLanguage: language,
+      isSpeedDialOpen: false
+    })
+  }
+
+  handleOpenSpeedDial = () => {
+    this.setState({
+      isSpeedDialOpen: true
+    })
+  }
+
+  handleCloseSpeedDial = () => {
+    this.setState({
+      isSpeedDialOpen: false
+    })
+  }
+
+  handleFlashCards = () => {
+    this.setState({
+      showModal: true,
+      isSpeedDialOpen: false,
+      modalComponent: 'FlashCards'
+    })
+  }
+
+  handleOriginalText = () => {
+    this.setState({
+      showModal: true,
+      isSpeedDialOpen: false,
+      modalComponent: 'OriginalText'
+    })
+  }
+
+
+  handleClose = () => {
+    this.setState({
+      showModal: false
+    })
+  }
+
+  componentWillUnmount() {
+    let iframes = [].slice.apply(document.querySelectorAll('iframe'));
+    let hypothesisURL ="";
+    for (let anIframe of iframes) {
+      if (anIframe.src.startsWith("https://hypothes.is")) {
+        anIframe.src = anIframe.src.split("&")[0]
+      }
+    }
+    let params = {
+      userId: this.props.userId,
+      storyId: this.props.stories.storyInfo._id,
+      savedVocabIds: this.props.sideBar.savedVocabIds,
+      savedWords: this.props.sideBar.savedWords
+    }
+    this.props.updateSavedWords(params);
+    //if(this.props.analytics.sessions.length > 0)this.props.endGrammarSearchSession();
+    this.props.leaveStories();
+    this.props.resetSTories(hypothesisURL.split("&")[0] + `&${Math.floor(Math.random()*100000)}`);
+    this.props.resetSideBar();
+    this.props.disableSideBarButton();
+  }
+
+  render() {
+    const {sideBar, stories} = this.props;
+    let iframes = [].slice.apply(document.querySelectorAll('iframe'));
+    for (let anIframe of iframes) {
+      if (anIframe.src.startsWith("https://hypothes.is")) {
+        anIframe.src += "&";
+        anIframe.src = anIframe.src.split("&")[0]
+      }
+    }
+    let text;
+    let vocab;
+    let grammar;
+    let searchWord = this.props.searchVocab.highlightedWord;
+    let title = "";
+    let author = "";
+
+    if(stories[this.state.selectedLanguage]) {
+      text = stories[this.state.selectedLanguage].storyText
+      vocab = stories[this.state.selectedLanguage].vocabList;
+      grammar = stories[this.state.selectedLanguage].grammarList;
+      switch(this.state.selectedLanguage){
+        case 'MODKR':
+          title = stories.storyInfo.titleKorn;
+          author = stories.storyInfo.authorKorn;
+          break;
+        case 'ENGSH':
+          title = stories.storyInfo.titleEng
+          author = stories.storyInfo.authorRom
+          break;
+        case 'MIDKR':
+          title = stories.storyInfo.titleKorn
+          author = stories.storyInfo.authorKorn
+          break;
+        default:
+          title = stories.storyInfo.titleKorn
+          author = stories.storyInfo.authorKorn
+      }
+    }
+    let modalComponent = null
+    switch(this.state.modalComponent){
+      case 'FlashCards':
+        modalComponent = <FlashCardsContainer vocabList={this.props.stories.MODKR.vocabList}/>
+        break;
+      case 'OriginalText':
+        modalComponent = <OriginalText url={this.props.stories.storyInfo.pdfUrl} selectedPages={this.props.stories.storyInfo.pagesSelected}/>
+        break;
+      default:
     }
 
-    handleOpenSpeedDial = () => {
-        this.setState({
-            isSpeedDialOpen: true
-        })
-    }
+    return (
+      stories[this.state.selectedLanguage] ?
+        <div className={'story-container'}>
+        <Modal
+          aria-labelledby="simple-modal-title"
+          aria-describedby="simple-modal-description"
+          open={this.state.showModal}
+          onClose={this.handleClose}
+        >
+          {modalComponent}
+        </Modal>
 
-    handleCloseSpeedDial = () => {
-        this.setState({
-            isSpeedDialOpen: false
-        })
-    }
-
-    handleFlashCards = (isFiltered) => {
-        this.setState({
-            showModal: true,
-            isSpeedDialOpen: false,
-            modalComponent: 'FlashCards',
-            openFiltered: isFiltered
-        })
-    }
-
-    handleFlashCardSave = (vocab) => {
-        let temp = this.state.savedVocabs;
-        temp.push(vocab);
-        this.setState({
-            savedVocabs: temp
-        })
-        let params = {
-            userId: this.props.userId,
-            storyId: this.props.stories.storyInfo._id,
-            savedVocabIds: temp,
-            savedWords: this.props.sideBar.savedWords
-        }
-        this.props.updateSavedWords(params);
-    }
-
-    handleFlashCardUnsave = (vocab) => {
-        let temp = this.state.savedVocabs;
-        let tempList = temp.filter(e => e !== vocab);
-        this.setState({
-            savedVocabs: tempList
-        })
-        let params = {
-            userId: this.props.userId,
-            storyId: this.props.stories.storyInfo._id,
-            savedVocabIds: tempList,
-            savedWords: this.props.sideBar.savedWords
-        }
-        this.props.updateSavedWords(params);
-    }
-
-    handleOriginalText = () => {
-        this.setState({
-            showModal: true,
-            isSpeedDialOpen: false,
-            modalComponent: 'OriginalText'
-        })
-    }
-
-
-    handleClose = () => {
-        this.setState({
-            showModal: false
-        })
-    }
-
-    componentWillUnmount() {
-        let iframes = [].slice.apply(document.querySelectorAll('iframe'));
-        let hypothesisURL = "";
-        for (let anIframe of iframes) {
-            if (anIframe.src.startsWith("https://hypothes.is")) {
-                anIframe.src = anIframe.src.split("&")[0]
-            }
-        }
-        //if(this.props.analytics.sessions.length > 0)this.props.endGrammarSearchSession();
-        this.props.leaveStories();
-        this.props.resetSTories(hypothesisURL.split("&")[0] + `&${Math.floor(Math.random() * 100000)}`);
-        this.props.resetSideBar();
-        this.props.disableSideBarButton();
-    }
-
-    render() {
-        const {sideBar, stories} = this.props;
-        let iframes = [].slice.apply(document.querySelectorAll('iframe'));
-        for (let anIframe of iframes) {
-            if (anIframe.src.startsWith("https://hypothes.is")) {
-                anIframe.src += "&";
-                anIframe.src = anIframe.src.split("&")[0]
-            }
-        }
-        let text;
-        let vocab;
-        let grammar;
-        let searchWord = this.props.searchVocab.highlightedWord;
-        let title = "";
-        let author = "";
-
-        if (stories[this.state.selectedLanguage]) {
-            text = stories[this.state.selectedLanguage].storyText
-            vocab = stories[this.state.selectedLanguage].vocabList;
-            grammar = stories[this.state.selectedLanguage].grammarList;
-            switch (this.state.selectedLanguage) {
-                case 'MODKR':
-                    title = stories.storyInfo.titleKorn;
-                    author = stories.storyInfo.authorKorn;
-                    break;
-                case 'ENGSH':
-                    title = stories.storyInfo.titleEng
-                    author = stories.storyInfo.authorRom
-                    break;
-                case 'MIDKR':
-                    title = stories.storyInfo.titleKorn
-                    author = stories.storyInfo.authorKorn
-                    break;
-                default:
-                    title = stories.storyInfo.titleKorn
-                    author = stories.storyInfo.authorKorn
-            }
-        }
-        let modalComponent = null
-        switch (this.state.modalComponent) {
-            case 'FlashCards':
-                modalComponent = <FlashCardsContainer savedVocabList={this.state.savedVocabs} vocabList={this.props.stories.MODKR.vocabList} storyTitle={this.state.storyTitle} cookies={this.props.cookies}
-                                                      handleSave={this.handleFlashCardSave} handleUnsave={this.handleFlashCardUnsave} openFiltered={this.state.openFiltered}/>
-                break;
-            case 'OriginalText':
-                modalComponent = <OriginalText url={this.props.stories.storyInfo.pdfUrl}
-                                               selectedPages={this.props.stories.storyInfo.pagesSelected}/>
-                break;
-            default:
-        }
-        return (
-            stories[this.state.selectedLanguage] ?
-                <div className={'story-container'}>
-                    <Modal
-                        aria-labelledby="simple-modal-title"
-                        aria-describedby="simple-modal-description"
-                        open={this.state.showModal}
-                        onClose={this.handleClose}
-                    >
-                        {modalComponent}
-                    </Modal>
-
-                    <div style={{top: "40vh", left: "45%", position: "absolute", display: "flex"}}>
-                        <ClipLoader
-                            sizeUnit={"px"}
-                            size={150}
-                            color={'#36D7B7'}
-                            loading={this.props.stories.isStoryLoading}
-                        />
-                    </div>
-                    <div>
-                        {this.props.stories.storyInfo ?
-                            <SideBar vocab={vocab} grammar={grammar} story={stories.storyTitle}
-                                     onResize={this.onResize}/> : null
-                        }
-                    </div>
-                    {stories.storyInfo ?
-                        <Story title={title}
-                               author={author}
-                               text={text}
-                               searchWord={searchWord}
-                               sideBar={sideBar}
-                               language={this.state.selectedLanguage}
-                               handleTranslate={this.handleTranslate}
-                               handleFlashCards={this.handleFlashCards}
-                               handleOriginalText={this.handleOriginalText}
-                               stories={this.props.stories}
-                        /> : null}
-                </div>
-                : null
-        );
-    }
+        <div style={{top: "40vh", left: "45%", position: "absolute", display: "flex"}}>
+          <ClipLoader
+            sizeUnit={"px"}
+            size={150}
+            color={'#36D7B7'}
+            loading={this.props.stories.isStoryLoading}
+          />
+        </div>
+        <div>
+          {this.props.stories.storyInfo ?
+            <SideBar vocab={vocab} grammar={grammar} story={stories.storyTitle}
+                     onResize={this.onResize}/> : null
+          }
+        </div>
+        {stories.storyInfo ?
+          <Story title={title}
+                 author={author}
+                 text={text}
+                 searchWord={searchWord}
+                 sideBar={sideBar}
+                 language={this.state.selectedLanguage}
+                 handleTranslate={this.handleTranslate}
+                 handleFlashCards={this.handleFlashCards}
+                 handleOriginalText={this.handleOriginalText}
+                 stories={this.props.stories}
+          /> : null}
+      </div>
+        : null
+    );
+  }
 }
 
 const mapStateToProps = state => (
-    {
-        stories: state.stories,
-        userId: state.auth.user.id,
-        sideBar: state.sideBar,
-        dashboard: state.dashboard,
-        analytics: state.analytics,
-        searchVocab: state.vocab
-    }
+  {
+    stories: state.stories,
+    userId: state.auth.user.id,
+    sideBar: state.sideBar,
+    dashboard: state.dashboard,
+    analytics: state.analytics,
+    searchVocab: state.vocab
+  }
 )
 
 const mapDispatchToProps = ({
-    getVocabforStory, getListOfSavedWords, initStory,
-    getSavedWords, leaveStories, enableSideBarButton, resetSTories, resetSideBar,
-    disableSideBarButton, updateSavedWords, enableLoading, disableLoading, saveHypothesisLink,
-    endGrammarSearchSession, getListOfSavedGrammars
+  getVocabforStory, getListOfSavedWords, initStory,
+  getSavedWords, leaveStories, enableSideBarButton, resetSTories, resetSideBar,
+  disableSideBarButton, updateSavedWords,enableLoading, disableLoading,saveHypothesisLink,
+  endGrammarSearchSession
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(StoriesContainer);
