2ae4d06dee787920bfb45ceed414df75aefceb5e
kessris <kessris@hotmail.com>
2020-10-27 18:39:21 -0700

    in progress of making the cells editable

--- client/src/components/instructor/EditStory/EditStory.js
+++ client/src/components/instructor/EditStory/EditStory.js
@@ -3,24 +3,57 @@ import { connect } from 'react-redux';
 import {
     initStory
 } from '../../../actions/stories';
+import EditStoryTable from './EditStoryTable'
+
+const headers = [
+    {
+        label: "Lines (Double-click to Edit)",
+        value: "text"
+    }
+];
 
 class EditStory extends Component {
     constructor(props) {
         super();
         this.state={
+            rowsKOR: [],
+            rowsENG: []
         }
     }
 
     componentWillMount() {
-        this.props.initStory(this.props.match.params.storyName)
+        this.props.initStory(this.props.match.params.storyName).then(() => {
+            let storyInfo = this.props.stories;
+            let kor = [];
+            let eng = [];
+
+            storyInfo.MODKR.storyText.forEach(line => {
+                kor.push({order_id: line.order_id, text: line.text, id: line._id})});
+            storyInfo.ENGSH.storyText.forEach(line => {
+                eng.push({order_id: line.order_id, text: line.text, id: line._id})});
+
+            this.setState({
+                rowsKOR: kor,
+                rowsENG: eng
+            });
+        });
     }
 
     render() {
-        console.log(this.props.stories)
-        const {stories} = this.props;
+        const {rowsKOR, rowsENG} = this.state;
+
         return (
             <div>
-                hi
+                <h1 style={{padding: '2% 5%'}}>KOREAN</h1>
+                <EditStoryTable
+                    tableHeaders={headers}
+                    list={rowsKOR}/>
+
+                <h1 style={{padding: '2% 5%'}}>ENGLISH</h1>
+                <EditStoryTable
+                    tableHeaders={headers}
+                    list={rowsENG}/>
+                <br/><br/>
             </div>
         );
     }
