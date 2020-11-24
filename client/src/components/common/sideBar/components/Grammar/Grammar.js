b85545f7fd2e6012106b4e01741cd137c3ac0e9f
kessris <kessris@hotmail.com>
2020-11-23 20:34:27 -0800

    vocab&grammar sidebar fetches from different fields now

--- client/src/components/common/sideBar/components/Grammar/Grammar.js
+++ client/src/components/common/sideBar/components/Grammar/Grammar.js
@@ -25,14 +25,14 @@ const scrollToPos = () => {
 const Grammar = (props) => {
   const {grammarList, classes} = props
   const pointerButton = {cursor: 'pointer', maxWidth: "116px", paddingLeft: "8px"}
-
   return(
     <Table>
       <TableHead>
         <TableRow>
-          <TableCell>Sentence</TableCell>
+          <TableCell>Korean</TableCell>
           <TableCell>Pattern</TableCell>
-          <TableCell>Context</TableCell>
+            <TableCell>Gloss</TableCell>
+          <TableCell>Here</TableCell>
             <TableCell> </TableCell>
         </TableRow>
       </TableHead>
@@ -42,8 +42,9 @@ const Grammar = (props) => {
             <TableRow key={"grammar_" + index}>
               <TableCell classes={{root: classes.cursor}}
                          style={{cursor: 'pointer', maxWidth: "116px", paddingRight: "25px"}}
-                         onClick={() =>{props.updateHighlightWord(grammar.sentence.trim(), "grammar"); scrollToPos()} }>{grammar.sentence}</TableCell>
+                         onClick={() =>{props.updateHighlightWord(grammar.korean.trim(), "grammar"); scrollToPos()} }>{grammar.korean}</TableCell>
                 <TableCell style={pointerButton} classes={{root: classes.cursor}}><div dangerouslySetInnerHTML={{ __html: grammar.pattern }} /></TableCell>
+                <TableCell style={pointerButton} classes={{root: classes.cursor}}><div dangerouslySetInnerHTML={{ __html: grammar.gloss }} /></TableCell>
               <TableCell style={pointerButton} classes={{root: classes.cursor}}>{grammar.here}</TableCell>
                 <TableCell onClick={ ()=> props.handleAddSavedGrammar(grammar)}><Tooltip disableFocusListener title="Save Grammar"><i style={pointerButton} className="material-icons">add</i></Tooltip></TableCell>
             </TableRow>
