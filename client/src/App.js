1f469f36d203de9ea99c50164c7bd0cec6d83bbe
S Park <spark23@students.cs.ubc.ca>
2021-09-09 12:54:06 -0700

    changed back what the Interline logo does

--- client/src/App.js
+++ client/src/App.js
@@ -8,7 +8,7 @@ import About from "./components/About/AboutContainer";
 import AboutNewBusu from "./KORN351/pages/Lessons/AboutNewBusu";
 import AboutNewPhonetics from "./KORN351/pages/Lessons/AboutNewPhonetics";
 import Cleaner from './components/Cleaner/CleanerContainer';
-import DashboardContainer from './components/dashboard/DashboardContainer';
+import Dashboard from "./KORN351/Dashboard";
 import DictionaryHangulContainer from "./KORN351/pages/Okpyeon/OkpyeonHangulContainer";
 import DictionaryLessonContainer from "./KORN351/pages/Okpyeon/OkpyeonLessonContainer";
 import DictionaryRadicalContainer from './KORN351/pages/Okpyeon/OkpyeonRadicalContainer';
@@ -150,7 +150,7 @@ class App extends Component {
                             <Route exact path="/dashboard/KORN351/lesson/:lesson/new-hanja-combos"
                                    component={NewHanjaCombos}/>
                             <Switch>
-                                <PrivateRoute path="/dashboard" component={DashboardContainer}/>
+                                <PrivateRoute path="/dashboard" component={Dashboard}/>
                                 <PrivateRoute path='/story' component={Story} cookies={this.props.cookies}/>
                                 <PrivateRoute path='/instructor410' component={Instructor}/>
                                 <PrivateRoute path='/instructor351' component={Instructor351}/>
