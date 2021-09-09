9f805115b04925cd6424147c1eb6273df6190434
S Park <spark23@students.cs.ubc.ca>
2021-09-09 13:05:16 -0700

    interline logo goes to dashboard instead of about

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -65,7 +65,7 @@ class Navbar extends Component {
                 <nav className="navbar navbar-expand-md navbar-dark bg-dark">
                     <SideBarButton toggleDrawer={this.toggleDrawer} isDisabled={this.props.sideBar.isButtonDisabled}/>
                     <div className="container">
-                        <Link className="navbar-brand" to="/about">
+                        <Link className="navbar-brand" to="/dashboard">
                             Interline Reader
                         </Link>
                         <button
@@ -101,7 +101,7 @@ class Navbar extends Component {
                                 <li className="nav-item">
                                     <Link className={currentPath === "/KORN351/tutorial" ? "selected-navbar" : "nav-link"}
                                           to="/KORN351/tutorial">
-                                        KORN351 User Guide
+                                        Tutorial
                                     </Link>
                                 </li>
                                 {/*{(Object.keys(user).length>=1 && !user.isStudent) && (*/}
