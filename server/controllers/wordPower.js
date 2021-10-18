656dded9a216e92876b7bc844aa71eae9e2e6cb1
S Park <spark23@students.cs.ubc.ca>
2021-10-18 01:02:00 -0700

    Looks like bugs where there were yemuns in the db but not showing up are fixed

--- server/controllers/wordPower.js
+++ server/controllers/wordPower.js
@@ -1,7 +1,7 @@
 const WordPower = require('../models/WordPower');
 const Yemun = require('../models/Yemun');
 
- function createWordPower(req, res) {
+function createWordPower(req, res) {
     // for (let obj of req.body) {
     //     const wordPower = new WordPower(obj);
     //     const newWordPower = await wordPower.save();
@@ -9,23 +9,23 @@ const Yemun = require('../models/Yemun');
     // return res.status(201).json("done");
 
     const wordPower = new WordPower(req.body);
-    WordPower.find({ lesson: req.body.lesson, hankul: req.body.hankul })
-    .exec()
-    .then((words) => {
-        if (words.length >= 1) {
+    WordPower.find({lesson: req.body.lesson, hankul: req.body.hankul})
+        .exec()
+        .then((words) => {
+            if (words.length >= 1) {
+                return res.status(409).json({
+                    message: "Word already exists",
+                });
+            } else {
+                const newWordPower = wordPower.save();
+                return res.status(201).json(newWordPower);
+            }
+        })
+        .catch((err) => {
             return res.status(409).json({
-                message: "Word already exists",
+                message: err,
             });
-        } else {
-            const newWordPower = wordPower.save();
-            return res.status(201).json(newWordPower);
-        }
-    })
-    .catch((err) => {
-        return res.status(409).json({
-            message: err,
         });
-    });
 }
 
 async function createYemun(req, res) {
@@ -40,7 +40,7 @@ async function createYemun(req, res) {
     // return res.status(201).json(newYemun);
 
     const yemun = new Yemun(req.body);
-    Yemun.find({ translation: req.body.translation })
+    Yemun.find({translation: req.body.translation})
         .exec()
         .then((yemuns) => {
             if (yemuns.length >= 1) {
@@ -57,6 +57,62 @@ async function createYemun(req, res) {
                 message: err,
             });
         });
+}
+
+function isHangul(str, options) {
+    if (
+        options !== undefined &&
+        options.removeSpace
+    )
+        str = str.replace(/(\s*)/g, "");
+
+    var result = [];
+
+    for (var i = 0; i < str.length; i++) {
+        var uniChar = str.charCodeAt(i);
+
+        if (
+            options !== undefined &&
+            options.onlyCombined
+        )
+            result.push(
+                uniChar >= 0xAC00 &&
+                uniChar <= 0xD7A3
+            );
+        else
+            result.push(
+                (
+                    uniChar >= 0x1100 &&
+                    uniChar <= 0x11FF
+                ) ||
+                (
+                    uniChar >= 0x3130 &&
+                    uniChar <= 0x318F
+                ) ||
+                (
+                    uniChar >= 0xAC00 &&
+                    uniChar <= 0xD7A3
+                )
+            );
+    }
+    return result;
+}
+
+function isPunctuation(str, options) {
+    if (
+        options !== undefined &&
+        options.removeSpace
+    )
+        str = str.replace(/(\s*)/g, "");
+
+    var result = [];
+
+    for (var i = 0; i < str.length; i++) {
+        let punctuationless = str.replace(/(~|`|!|@|#|$|%|^|&|\*|\(|\)|{|}|\[|\]|;|:|\"|'|<|,|\.|>|\?|\/|\\|\||-|_|\+|=)/g, "")
+        let finalString = punctuationless.replace(/\s{2,}/g, " ");
+        result.push(finalString);
+    }
+    return result;
 }
 
 async function list(req, res) {
@@ -80,8 +136,20 @@ async function list(req, res) {
         const preMatchedExamples = await Yemun.find({hanqcaMatch: {$in: hanqcaMatcher}});
 
         for (const yemun of preMatchedExamples) {
-            const hanqcaArr = yemun.hanqcaMatch.join("");
-            if (hanqcaArr.includes(hanqca)) {
+            const yemunHanqcaArr = yemun.hanqcaMatch.join("").replace(/\s/g, '').trim();
+            let hanqcaInWord = [];
+            let punc = isPunctuation(wordPower.hanqca);
+            for (let i = 0; i < punc[0].length; i++) {
+                let hangulBool = isHangul(punc[0][i]);
+                if (hangulBool.includes(false)) {
+                    hanqcaInWord.push(punc[0][i]);
+                }
+            }
+            let finalWordPowerHanqcaArr = hanqcaInWord.join("").replace(/\s/g, '').trim();
+            console.log("the final");
+            console.log(finalWordPowerHanqcaArr);
+            console.log(hanqca);
+            if (yemunHanqcaArr.includes(finalWordPowerHanqcaArr)) {
                 matchedExamples.push(yemun);
             }
         }
