9a3b304cb157d90358546cfed603393510b6dbc0
msintaha <msintaha94@gmail.com>
2021-10-18 21:41:30 -0700

    Pull data from new_busu table in New Busu page

--- client/src/constants/351-action-types.js
+++ client/src/constants/351-action-types.js
@@ -1,5 +1,6 @@
 // Okpyeon
 export const GET_CHARACTERS = "GET_CHARACTERS";
+export const GET_NEW_BUSU = "GET_NEW_BUSU";
 export const GET_RADICALS = "GET_RADICALS";
 export const GET_PHONETICS = "GET_PHONETICS";
 export const GET_EUM_FILTERS = "GET_EUM_FILTERS";
