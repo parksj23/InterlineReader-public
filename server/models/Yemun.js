1ef5dce3deb05e06cb9b17b887e71b86d857da27
S Park <spark23@students.cs.ubc.ca>
2021-10-29 19:30:37 -0700

    just pushing some leftover changes, not really important because they are already in other branches

--- server/models/Yemun.js
+++ server/models/Yemun.js
@@ -2,12 +2,14 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const YemunSchema = new Schema({
+	lesson: { type: Number },
 	koreanSentence: { type: String },
 	simpleHanqca: { type: String },
 	hanqcaizedSentence: { type: String },
 	translation: { type: String },
 	hanqcaMatch: { type: Array, default: [] },
-}, { toJSON: { virtuals: true, getters: true }});
+// }, { toJSON: { virtuals: true, getters: true }});
+}, { toJSON: { virtuals: true, getters: true }, collection: "yemunTEST"});
 
 const Yemun = mongoose.model('yemun', YemunSchema);
 module.exports = Yemun;
