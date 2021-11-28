6eaf9c0b0696ed82e3cb75af9793d89e95154dd2
S Park <spark23@students.cs.ubc.ca>
2021-11-27 17:18:25 -0800

    reloads for NewHanjaContainer, EditOkpyeon, and NewBusuContainer are reduced

--- client/src/KORN351/components/InstructorPanel/EditOkpyeon.js
+++ client/src/KORN351/components/InstructorPanel/EditOkpyeon.js
@@ -22,7 +22,7 @@ class EditOkpyeon extends Component {
 
     componentDidMount() {
         const reloadCount = sessionStorage.getItem('reloadCount');
-        if(reloadCount < 2) {
+        if(reloadCount < 1) {
             sessionStorage.setItem('reloadCount', String(reloadCount + 1));
             window.location.reload();
         } else {
