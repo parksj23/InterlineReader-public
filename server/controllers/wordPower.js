62ae8096bae8f1ac322aaf743e960485b1fe60a4
S Park <spark23@students.cs.ubc.ca>
2021-12-01 18:24:30 -0800

    cleaning

--- server/controllers/wordPower.js
+++ server/controllers/wordPower.js
@@ -2,62 +2,6 @@ const WordPower = require('../models/WordPower');
 const Yemun = require('../models/Yemun');
 const {getHanqcaMatchArray, isHangul, isPunctuation} = require('../helpers/hanjaMatchGenerator');
 
-// function isHangul(str, options) {
-//     if (
-//         options !== undefined &&
-//         options.removeSpace
-//     )
-//         str = str.replace(/(\s*)/g, "");
-//
-//     var result = [];
-//
-//     for (var i = 0; i < str.length; i++) {
-//         var uniChar = str.charCodeAt(i);
-//
-//         if (
-//             options !== undefined &&
-//             options.onlyCombined
-//         )
-//             result.push(
-//                 uniChar >= 0xAC00 &&
-//                 uniChar <= 0xD7A3
-//             );
-//         else
-//             result.push(
-//                 (
-//                     uniChar >= 0x1100 &&
-//                     uniChar <= 0x11FF
-//                 ) ||
-//                 (
-//                     uniChar >= 0x3130 &&
-//                     uniChar <= 0x318F
-//                 ) ||
-//                 (
-//                     uniChar >= 0xAC00 &&
-//                     uniChar <= 0xD7A3
-//                 )
-//             );
-//     }
-//     return result;
-// }
-//
-// function isPunctuation(str, options) {
-//     if (
-//         options !== undefined &&
-//         options.removeSpace
-//     )
-//         str = str.replace(/(\s*)/g, "");
-//
-//     var result = [];
-//
-//     for (var i = 0; i < str.length; i++) {
-//         let punctuationless = str.replace(/(~|`|!|@|#|$|%|^|&|\*|\(|\)|{|}|\[|\]|;|:|\"|'|<|,|\.|>|\?|\/|\\|\||-|_|\+|=)/g, "")
-//         let finalString = punctuationless.replace(/\s{2,}/g, " ");
-//         result.push(finalString);
-//     }
-//     return result;
-// }
-
 async function createWordPower(req, res) {
     req.body.hanqca = req.body.hanqca.normalize('NFC');
 
