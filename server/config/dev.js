62aaf94584fb56ecc51a3c3f8d18d10e25018a62
S Park <spark23@students.cs.ubc.ca>
2022-10-19 14:35:57 -0700

    initial commit

--- server/config/dev.js
+++ server/config/dev.js
@@ -1,7 +1,5 @@
 module.exports = {
-//	mongoURI: 'mongodb://ubcreader:britishcolumbia1324!@ds155862.mlab.com:55862/ubcreadertesting',
-//  mongoURI: 'mongodb+srv://ubcreader:britishcolumbia1324!@cluster0-cgsxj.mongodb.net/interlineReaderTestDb?retryWrites=true&w=majority',
-	mongoURI: 'mongodb://localhost:27017/interlinereader',
+    mongoURI: 'mongodb://localhost:27017/interlinereader',
 	secretOrKey: 'secret',
 	sendGridKey: 'YOUR_SENDGRID_KEY',
 	redirectDomain: 'http://127.0.0.1:5050/', // e.g. http://localhost:3000
